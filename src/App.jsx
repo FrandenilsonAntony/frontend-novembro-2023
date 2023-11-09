5b2ce424699c0bf4ffe27ec7d2e37a3fc0be368e
Frandenilson Antony <iabyantony88@gmail.com>
2023-11-09 03:42:59 +0000

    "Feat: Created stylization"

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import './App.css'
 function App() {
   return (
     <>
-     app
+     <div className="card">
+      <h2>Empresa</h2>
+      <h3>Tribunal Regional Eleitoral</h3>
+      <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"/>
+     </div>
 
     </>
   )
