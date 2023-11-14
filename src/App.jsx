c96cffb77190a950868eeff57278d96fcd2706f4
Frandenilson Antony <iabyantony88@gmail.com>
2023-11-14 03:43:51 +0000

    "Feat:Implementation of `Map` for loading `<Card />` and  implementation property implementation in `<Card />`"

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,28 @@ import './App.css'
 import Card from './components/Card/Card';
 
 function App() {
+  const item1 = {
+    name: "Rick Sanchez",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+  }
+
+  const item2 = {
+    name: "Morty Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+  }
+
+  const item3 = {
+    name: "Summer Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+  }
+
+  const items = [item1, item2, item3]
+
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
-      <br />
-      <Card />
-
+      {items.map(function (element) {
+        return <Card item={element} />
+      })}
     </>
   )
 }
