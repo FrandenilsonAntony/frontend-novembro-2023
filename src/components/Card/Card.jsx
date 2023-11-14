88f5713c8aef6a618728194e7e5783e1d4e8ad4d
Frandenilson Antony <iabyantony88@gmail.com>
2023-11-14 23:13:40 +0000

    "Feat:implementação do `Array` no `<Card />` and `Map`"

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,17 @@
+import Tag from "../Tag/Tag"
+
 export default function Card(props) {
-    const item = props.item
-  
-    return <div className="card">
-      <h2>{item.name}</h2>
-      <div className="tags">
-        <div className="tag">Status: Vivo</div>
-        <div className="tag">Espécie: Humana</div>
-        <div className="tag">Origem: Terra C-137</div>
-      </div>
-      <img src={item.imageUrl} />
+  const item = props.item
+
+  const tags = item.tags || []
+
+  return <div className="card">
+    <h2>{item.name}</h2>
+    <div className="tags">
+      {tags.map(function (element) {
+        return <Tag text={element} />
+      })}
     </div>
-  }
+    <img src={item.imageUrl} />
+  </div>
+}
